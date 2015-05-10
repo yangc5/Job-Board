b10f23ba50653e5e76070d513c04ba7d81b6409d
Chen Yang <yangc5@mail.uc.edu>
2015-05-10 17:51:37 -0500

    db change added a contact column

--- db/migrate/20150503202635_create_jobs.rb
+++ db/migrate/20150503202635_create_jobs.rb
@@ -3,6 +3,7 @@ class CreateJobs < ActiveRecord::Migration
     create_table :jobs do |t|
       t.text :title
       t.text :description
+      t.text :contact
       t.timestamps null: false
     end
   end
